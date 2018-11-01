596cac7714e0f18f01c546dea3ac2734cc5c4543
Advent21 <43126138+Advent21@users.noreply.github.com>
2018-11-01 00:25:31 -0300

    reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -3,31 +3,32 @@
 
 using namespace std;
 
-int reverse (int n, int r)
-	{
-		int m;
-		
-		if (n>0)
-		{
-		
-		m = n%10;
-		//n = n/10;	
-		r = r*10+m;
-		
-		return reverse ((n/10),r);
-			
-		}
-		else
-		 
-		 return r;
-	}
-	
-int main ()
+int reverse(int n, int r)
 {
-	int g=0;
-	int num = 1247878;
-	
-	int number = reverse(num,g);
-	
+	int m;
+
+	if (n>0)
+	{
+
+		m = n % 10;	
+		r = r * 10 + m;
+
+		return reverse((n / 10), r);
+
+	}
+	else
+
+		return r;
+}
+
+int main()
+{
+	int a = 0;
+	int num = 0;
+	cout << "Enter a number" << endl;
+	cin >> num;
+	int number = reverse(num, a);
 	cout << number << endl;
+	system("pause");
+
 }
